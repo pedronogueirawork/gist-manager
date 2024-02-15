c12ef01efce21edfeb2f1ef472815d8f836a6582
Pedro Nogueira <pnogueirawork@gmail.com>
2024-02-15 15:26:31 +0000

    feat: git workflow

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -13,18 +13,21 @@ const authTokenLogin = async () => {
 
 <template>
   <div class="main-wrapper flex flex-col items-center justify-center">
-    <div class="mb-6 text-3xl">The Great Gist Manager</div>
-    <div class="token-box">
-      <div class="border rounded border-slate-300 p-2 mb-3">
-        <input
-          type="text"
-          class="w-full bg-transparent p-2 border border-slate-300 rounded focus:outline-none mb-2"
-          placeholder="Your token here"
-          v-model="authToken"
-        />
-        <button class="w-full p-2 bg-green-500 rounded text-white" @click="authTokenLogin">
-          Login using token
-        </button>
+    <img src="../assets/github.svg" class="mb-12" />
+    <div class="bg-white p-12 rounded">
+      <div class="mb-6 text-3xl text-center">The Great Gist Manager</div>
+      <div class="token-box">
+        <div class="border rounded border-slate-300 p-2 mb-3">
+          <input
+            type="text"
+            class="w-full bg-transparent p-2 border border-slate-300 rounded focus:outline-none mb-2"
+            placeholder="Your token here"
+            v-model="authToken"
+          />
+          <button class="w-full p-2 bg-green-500 rounded text-white" @click="authTokenLogin">
+            Login using token
+          </button>
+        </div>
       </div>
     </div>
   </div>
